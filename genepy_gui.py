725133d0134c84039bb63a17926085466dc3144f
unknown <hkim42@bu.edu>
2020-04-27 08:35:00 -0400

    minimally viable gui, separated main to test_gui.py

--- genepy_gui.py
+++ genepy_gui.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import genepy
+from genepy import genepy
 
 class genepy_gui:
     def __init__(self):
@@ -9,7 +9,8 @@ class genepy_gui:
                        'SOME_REVERSE']
         self.rules2 = ['MORETHAN', 'EXACTLY', 'THEN', 'WITH', 'ALL_AFTER', 'SOME_AFTER', 'ALL_BEFORE',
                        'SOME_BEFORE', 'ALL_NEXTTO', 'SOME_NEXTTO', 'REPRESSES', 'INDUCES', 'DRIVES']
-        
+        self.mygp = genepy(1)
+
         self.window = tk.Tk()
 
         self.ent_N = tk.Entry()
@@ -17,12 +18,16 @@ class genepy_gui:
         self.ent_N.grid(row=0, column=0)
 
         self.btn_console = tk.Button(text='Launch Console', width=16, height=2)
-        self.btn_console.grid(row=0, column=2)
+        self.btn_console.grid(row=0, column=1)
         self.btn_console.bind('<Button-1>', (lambda event: self.handle_btn_console()))
-        
+
+        self.ent_sol = tk.Entry()
+        self.ent_sol.insert(0, 'Enter No. Solutions')
+        self.ent_sol.grid(row=0, column=2)
+
         self.btn_run = tk.Button(text='Run Solver', width=16, height=2)
         self.btn_run.grid(row=0, column=4)
-        self.btn_run.bind('<Button-1>', self.handle_btn_run())
+        self.btn_run.bind('<Button-1>', (lambda event: self.handle_btn_run()))
 
         ## Grid for Rules ##########################################################
         row_start = 1
@@ -34,7 +39,7 @@ class genepy_gui:
         row_rule = row_rule+1
         self.rule_add_btn(self.window, row_rule, column_rule, 'MORETHAN')
         row_rule = row_rule+1
-        self.rule_add_btn(self.window, row_rule, column_rule, 'LESSTHAN')
+        self.rule_add_btn(self.window, row_rule, column_rule, 'CONTAINS')
         row_rule = row_rule+1
         self.rule_add_btn(self.window, row_rule, column_rule, 'EXACTLY')
 
@@ -57,6 +62,8 @@ class genepy_gui:
         self.rule_add_btn(self.window, row_rule, column_rule, 'SOME_AFTER')
         row_rule = row_rule+1
         self.rule_add_btn(self.window, row_rule, column_rule, 'ALL_BEFORE')
+        row_rule = row_rule+1
+        self.rule_add_btn(self.window, row_rule, column_rule, 'SOME_BEFORE')
         row_rule = row_rule+1
         self.rule_add_btn(self.window, row_rule, column_rule, 'ALL_NEXTTO')
         row_rule = row_rule+1
@@ -101,7 +108,7 @@ class genepy_gui:
         _frm = tk.Frame(master=self.window3, width=300, height=300)
         _frm.pack(expand=True)
         self._canvas = tk.Canvas(master=_frm, width=300, height=300, scrollregion=(0,0,500,500), background='white')
-        
+
         hbar = tk.Scrollbar(_frm, orient='horizontal')
         hbar.pack(side='bottom', fill='x')
         hbar.config(command=self._canvas.xview)
@@ -112,7 +119,7 @@ class genepy_gui:
 
         self._canvas.config(xscrollcommand=hbar.set, yscrollcommand=vbar.set)
         self._canvas.pack(expand=True)
-        
+
         self.window3.mainloop()
         self.window3_open = False
 
@@ -129,10 +136,57 @@ class genepy_gui:
                 self._canvas.create_text(20, self.window3_text_location, anchor='w',
                                          text=self._ent0.get()+'    '+txt+'    '+self._ent1.get())
             else:
-                raise Exception('something went very wrong here')
-            
+                raise Exception('error0')
+
             self.window3_text_location = self.window3_text_location+20
-        
+
+        if txt == 'MORETHAN':
+            self.mygp.morethan(self._ent0.get(), int(self._ent1.get()))
+        elif txt == 'CONTAINS':
+            self.mygp.contains(self._ent0.get())
+        elif txt == 'EXACTLY':
+            self.mygp.exactly(self._ent0.get(), int(self._ent1.get()))
+        elif txt == 'THEN':
+            self.mygp.then(self._ent0.get(), self._ent1.get())
+        elif txt == 'WITH':
+            self.mygp.witH(self._ent0.get(), self._ent1.get())
+        elif txt == 'STARTSWITH':
+            self.mygp.startswith(self._ent0.get())
+        elif txt == 'ENDSWITH':
+            self.mygp.endswith(self._ent0.get())
+        elif txt == 'ALL_AFTER':
+            self.mygp.all_after(self._ent0.get(), self._ent1.get())
+        elif txt == 'SOME_AFTER':
+            self.mygp.some_after(self._ent0.get(), self._ent1.get())
+        elif txt == 'ALL_BEFORE':
+            self.mygp.all_before(self._ent0.get(), self._ent1.get())
+        elif txt == 'SOME_BEFORE':
+            self.mygp.some_before(self._ent0.get(), self._ent1.get())
+        elif txt == 'ALL_NEXTTO':
+            self.mygp.all_nextto(self._ent0.get(), self._ent1.get())
+        elif txt == 'SOME_NEXTTO':
+            self.mygp.some_nextto(self._ent0.get(), self._ent1.get())
+        elif txt == 'ALL_FORWARD_IF':
+            self.mygp.all_forward_if(self._ent0.get())
+        elif txt == 'ALL_REVERSE_IF':
+            self.mygp.all_reverse_if(self._ent0.get())
+        elif txt == 'SOME_FORWARD':
+            self.mygp.some_forward(self._ent0.get())
+        elif txt == 'SOME_REVERSE':
+            self.mygp.some_reverse(self._ent0.get())
+        elif txt == 'ALL_FORWARD':
+           self.mygp.all_forward()
+        elif txt == 'ALL_REVERSE':
+            self.mygp.all_reverse()
+        elif txt == 'REPRESSES':
+            self.mygp.represses(self._ent0.get(), self._ent1.get())
+        elif txt == 'INDUCES':
+            self.mygp.induces(self._ent0.get(), self._ent1.get())
+        elif txt == 'DRIVES':
+            self.mygp.drives(self._ent0.get(), self._ent1.get())
+        else:
+            raise Exception('error1')
+
         window.destroy()
 
     # opens a window after the user clicks a rule
@@ -142,7 +196,7 @@ class genepy_gui:
 
         self._ent0 = tk.Entry(master=window2)
         self._ent0.insert(0, 'Enter Part Name')
-        
+
         _lbl = tk.Label(master=window2, text=txt)
 
         self._ent1 = tk.Entry(master=window2)
@@ -158,7 +212,7 @@ class genepy_gui:
             _lbl.pack()
             self._ent1.pack()
         else:
-            raise Exception('something went very wrong here')
+            raise Exception('error2')
 
         btn_add_rule = tk.Button(master=window2, text='Add Rule')
         btn_add_rule.bind('<Button-1>', (lambda event: self.handle_btn_add(window2, txt)))
@@ -167,7 +221,21 @@ class genepy_gui:
         window2.mainloop()
 
     def handle_btn_run(self):
-        pass
+        self.mygp.change_length(int(self.ent_N.get()))
+        outputs = self.mygp.generate(int(self.ent_sol.get()))
+
+        buf = ''
+        for sequence in outputs:
+            for ii in range(0, len(sequence)):
+                buf = buf + sequence[ii]
+                if ii < len(sequence)-1:
+                    buf = buf + ' -> '
+            self._canvas.create_text(20, self.window3_text_location, anchor='w', text=buf)
+            self.window3_text_location = self.window3_text_location+20
+            buf = ''
+
+        for x in outputs:
+            print(x)
 
     def rule_add_lbl(self, win, _row, _col, txt):
         frm_rule = tk.Frame(master=win, borderwidth=1)
@@ -181,6 +249,3 @@ class genepy_gui:
         _btn = tk.Button(master=frm_rule, text=txt, width=16, height=2)
         _btn.bind('<Button-1>', (lambda event: self.handle_btn_rule(txt)))
         _btn.pack()
-
-if __name__ == '__main__':
-    foo = genepy_gui()
---
+++ test_gui.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import tkinter as tk
+from genepy import genepy
+from genepy_gui import genepy_gui
+
+if __name__ == '__main__':
+    foo = genepy_gui()
